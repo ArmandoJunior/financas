bdc11dbebb96a3c029c57cdea24f1d829795a620
Armando Junior <armando.ads@icloud.com>
2018-01-02 19:12:13 -0200

    primeiro comit

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: armandojrn
+ * Date: 02/01/2018
+ * Time: 08:34
+ */
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'financas',
+        'username' => 'root',
+        'password' => 'jr120777',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
